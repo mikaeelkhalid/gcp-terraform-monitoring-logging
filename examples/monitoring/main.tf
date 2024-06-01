31baf150bb71a2fbac08d68d5ef1baf1510cf3cc
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2024-06-01 12:42:20 +0200

    add monitoring module example

---
+++ examples/monitoring/main.tf
@@ -0,0 +1,23 @@
+module "monitoring" {
+  source     = "../../modules/monitoring"
+
+  project_id = "project-id"
+  project_region = "project-region"
+
+  uptime_checks = [
+    {
+      name     = "uptime-check-name"
+      hostname = "mikaeels.com"
+      path     = "/health"
+      period   = 60
+      timeout  = 10
+    }
+  ]
+
+  notification_channels = [
+    {
+      type  = "email"
+      email = "iam@mikaeels.com"
+    }
+  ]
+}
